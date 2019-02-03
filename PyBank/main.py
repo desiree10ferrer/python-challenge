825531ad30e5668080b9bbc1871f890533f76d25
blueskies <desireec.ferrer@gmail.com>
2019-02-03 11:59:53 -0500

    Homework 3

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,58 @@
+import os
+import csv
 
+csvpath = os.path.join('..', 'PyBank', 'budget_data.csv')
+total = 0
+months = []
+total_profit_loss = []   
+max_change = 0
+min_change = 0
+change = []
+min_i = 0
+max_i = 0
+
+with open(csvpath, newline='') as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+
+    for row in csv.reader(csvfile):
+        total_profit_loss.append(int(row[1]))
+        months.append(row[0]) 
+        months_total = len(months)
+        total += int(row[1])
+    
+    for i in range(1, len(total_profit_loss)):
+        change.append(total_profit_loss[i] - total_profit_loss[i-1])
+        average_change = round(sum(change)/len(change),2) 
+        max_change = max(change)
+        min_change = min(change)
+    
+    for i in range(1, len(total_profit_loss)):
+        if total_profit_loss[i] > total_profit_loss[max_i]:
+           max_i = i
+        max_months = months[max_i]
+        if total_profit_loss[i] < total_profit_loss[min_i]:
+           min_i = i
+        min_months = months[min_i]
+
+    print(f"Financial Analysis")
+    print(f"----------------------------")
+    print(f"Total Months: {str(months_total)}")
+    print(f"Total: ${(total)}")
+    print(f"Average  Change: ${(average_change)}")
+    print(f"Greatest Increase in profits: {max_months}, (${max_change})")
+    print(f"Greatest Decrease in profits: {min_months}, (${(min_change)})")
+
+output_path = os.path.join("..", "PyBank", "results.txt")
+
+with open(output_path, 'w', newline='') as datafile:
+
+    csvwriter = csv.writer(datafile, delimiter=',')
+    csvwriter.writerow([(f"Financial Analysis")])
+    csvwriter.writerow([(f"----------------------------")])
+    csvwriter.writerow([(f"Total Months: {str(months_total)}")])
+    csvwriter.writerow([(f"Total: ${(total)}")])
+    csvwriter.writerow([(f"Average  Change: ${(average_change)}")])
+    csvwriter.writerow([(f"Greatest Increase in profits: {max_months} (${max_change})")])
+    csvwriter.writerow([(f"Greatest Decrease in profits: {min_months} (${(min_change)})")])
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,53 @@
+import os
+import csv
+from collections import Counter
 
+csvpath = os.path.join('..', 'PyPoll', 'election_data.csv') 
+
+votes = []
+candidate = []
+w = 0
+with open(csvpath, newline="") as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+    for row in csv.reader(csvfile):
+        votes.append(row[0])
+        candidate.append(row[2])
+    vote_count = len(votes)
+    counter = Counter(candidate)
+    candidate_name = list(counter.keys())
+    individual_votes = list(counter.values())
+
+    print(f"Election Results")
+    print(f"----------------------------")
+    print(f"Total Votes: {(vote_count)}")
+    print(f"----------------------------")
+
+    for i in range (len(candidate_name)):
+        per = (individual_votes[i]/vote_count)*100   
+        print(f"{candidate_name[i]}: {format(per,'.3f')}% ({individual_votes[i]})")
+
+    for i in range(1, len(individual_votes)):
+        if individual_votes[i] > individual_votes[w]:
+            w = i
+        winner = candidate_name[w]
+    print(f"----------------------------")
+    print(f"Winner: {winner}")
+    print(f"----------------------------")
+
+output_path = os.path.join("..", "PyPoll", "results.txt")
+
+with open(output_path, 'w', newline='') as datafile:
+
+    csvwriter = csv.writer(datafile, delimiter=',')
+    csvwriter.writerow([(f"Election Results")])
+    csvwriter.writerow([(f"----------------------------")])
+    csvwriter.writerow([(f"Total Votes: {(vote_count)}")])
+    csvwriter.writerow([(f"----------------------------")])
+    for i in range (len(candidate_name)):
+        per = (individual_votes[i]/vote_count)*100  
+        csvwriter.writerow([(f"{candidate_name[i]}: {format(per,'.3f')}% ({individual_votes[i]})")])
+    csvwriter.writerow([(f"----------------------------")])
+    csvwriter.writerow([(f"Winner: {winner}")])
+    csvwriter.writerow([(f"----------------------------")])
